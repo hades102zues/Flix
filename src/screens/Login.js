627bc5c4355792e8c0c1ce644001ffd5379c6aa9
Joshua Wiggins <joshua_kar@hotmail.com>
2019-01-08 10:31:25 -0400

    achieved 2way binding on the form

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,78 +1,84 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, StatusBar, Button, TouchableNativeFeedback} from 'react-native';
+import React, { Component } from "react";
+import {
+	View,
+	Text,
+	StyleSheet,
+	StatusBar,
+	Button,
+	TouchableNativeFeedback
+} from "react-native";
 
-import Container from '../components/Container/Container';
-import LoginForm from '../containers/LoginForm/LoginForm';
+import Container from "../components/Container/Container";
+import LoginForm from "../containers/LoginForm/LoginForm";
 
 class Login extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isAuthStateLogin: true
+		};
+	}
 
-  constructor(props) {
-  	super(props);
-  	this.state = {
-  		isAuthStateLogin : true
-  	};
-  }
+	toggleAuthState = () => {
+		this.setState({ isAuthStateLogin: !this.state.isAuthStateLogin });
+	};
 
-  toggleAuthState = () => {
-  	this.setState({isAuthStateLogin : !this.state.isAuthStateLogin});
-  }
+	render() {
+		let authText =
+			this.state.isAuthStateLogin === true ? "SignUp" : "Login";
+		let authState =
+			this.state.isAuthStateLogin === true ? "login" : "signup";
 
-  render() {
+		return (
+			<Container>
+				<View style={styles.header}>
+					<Text style={styles.welcomeText}>Welcome Back,</Text>
 
-  	let authText = this.state.isAuthStateLogin === true ? 'SignUp' : 'Login';
-  	let authState = this.state.isAuthStateLogin === true ? 'login' : 'signup';
+					<TouchableNativeFeedback onPress={this.toggleAuthState}>
+						<Text style={styles.authText}>{authText}</Text>
+					</TouchableNativeFeedback>
+				</View>
 
-  	return (
-  		<Container>
-			<View style={styles.header}>
-				<Text style={styles.welcomeText}>Welcome Back,</Text>
+				<View>
+					<Text style={{ color: "#ccc" }}>
+						Enter your details to continue
+					</Text>
+				</View>
 
-				<TouchableNativeFeedback onPress={this.toggleAuthState}>
-					<Text style={styles.authText}>{authText}</Text>
-				</TouchableNativeFeedback>
-			</View>
-
-			<View>
-				<Text style={{color:"#ccc"}}>Enter your details to continue</Text>
-			</View>
-
-			<View style={styles.formCard}>
-				<View style={styles.form}>
-		    		<LoginForm authState={authState} {...this.props}/>
-		    	</View>
-			</View>  
-		</Container>
-  	); 
-  }
+				<View style={styles.formCard}>
+					<View style={styles.form}>
+						<LoginForm authState={authState} {...this.props} />
+					</View>
+				</View>
+			</Container>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
 	header: {
-		flexDirection: 'row',
-		marginTop : StatusBar.currentHeight + 40,
+		flexDirection: "row",
+		marginTop: StatusBar.currentHeight + 40
 	},
 	welcomeText: {
-		width: '50%',
-		fontWeight: '700',
+		width: "50%",
+		fontWeight: "700",
 		fontSize: 34,
-		color:'#333'
+		color: "#333"
 	},
-	authText : {
-		width: '50%', 
-		textAlign:'right',
-		color: '#ec407a',
+	authText: {
+		width: "50%",
+		textAlign: "right",
+		color: "#ec407a"
 	},
-	formCard:{
-		alignItems: 'center'
+	formCard: {
+		alignItems: "center"
 	},
 	form: {
-		width:'90%',
+		width: "90%",
 		marginTop: 50,
 		paddingVertical: 25
-	},
-
+	}
 });
 
 export default Login;
-
-
--- src/screens/NavigationApp.js
+++ src/screens/NavigationApp.js
@@ -1,77 +1,83 @@
-import React from 'react';
+import React from "react";
 import {
 	createStackNavigator,
 	createBottomTabNavigator,
 	createAppContainer
-} from 'react-navigation';
+} from "react-navigation";
 
-import ProfileScreen from './Profile';
-import HomeScreen from './Home';
-import ViewPageScreen from './ViewPage';
-import CheckoutScreen from './Checkout';
-import ConfirmPurchaseScreen from './ConfirmPurchase';
-import LoginScreen from './Login';
+import ProfileScreen from "./Profile";
+import HomeScreen from "./Home";
+import ViewPageScreen from "./ViewPage";
+import CheckoutScreen from "./Checkout";
+import ConfirmPurchaseScreen from "./ConfirmPurchase";
+import LoginScreen from "./Login";
 
-import Icon from 'react-native-vector-icons/Ionicons';
-
+import Icon from "react-native-vector-icons/Ionicons";
 
 const HomeStack = createStackNavigator({
-	Home : {
-		screen : HomeScreen,
-		navigationOptions : {
-			header : null,
-		},
+	Home: {
+		screen: HomeScreen,
+		navigationOptions: {
+			header: null
+		}
 	},
-	Viewer : ViewPageScreen,
+	Viewer: ViewPageScreen
 });
 
 const CartStack = createStackNavigator({
-	Checkout : {
-		screen : CheckoutScreen,
-		navigationOptions : {
-			header : null,
-		},
+	Checkout: {
+		screen: CheckoutScreen,
+		navigationOptions: {
+			header: null
+		}
 	},
-	ConfirmPurchase : ConfirmPurchaseScreen
+	ConfirmPurchase: ConfirmPurchaseScreen
 });
 
-const TabApp = createBottomTabNavigator({
-	Profile : {
-		screen: ProfileScreen,
-		navigationOptions : {
-			tabBarIcon : (props) => <Icon name="md-person" size={24} color={props.tintColor} />	
+const TabApp = createBottomTabNavigator(
+	{
+		Profile: {
+			screen: ProfileScreen,
+			navigationOptions: {
+				tabBarIcon: props => (
+					<Icon name="md-person" size={24} color={props.tintColor} />
+				)
+			}
 		},
-		
-	},
-	Home : {
-		screen : HomeStack,
-		navigationOptions : {
-			tabBarIcon : (props) => <Icon name="md-home" size={24} color={props.tintColor} />	
+		Home: {
+			screen: HomeStack,
+			navigationOptions: {
+				tabBarIcon: props => (
+					<Icon name="md-home" size={24} color={props.tintColor} />
+				)
+			}
 		},
+		Cart: {
+			screen: CartStack,
+			navigationOptions: {
+				tabBarIcon: props => (
+					<Icon name="md-cart" size={24} color={props.tintColor} />
+				)
+			}
+		}
 	},
-	Cart : { 
-		screen : CartStack,
-		navigationOptions : {
-			tabBarIcon : (props) => <Icon name="md-cart" size={24} color={props.tintColor} />	
-		},
+	{
+		initialRouteName: "Home"
+	}
+);
+
+const EntryStack = createStackNavigator(
+	{
+		Login: LoginScreen,
+		Tab: TabApp
 	},
-
-},
-{
-	initialRouteName : 'Home',
-});
-
-const EntryStack = createStackNavigator({
-	Login : LoginScreen,
-	Tab : TabApp,
-},
-{
-	initialRouteName : 'Login',
-	defaultNavigationOptions : {
-		header : null,
-	},
-});
-
+	{
+		initialRouteName: "Login",
+		defaultNavigationOptions: {
+			header: null
+		}
+	}
+);
 
 const NavigationApp = createAppContainer(EntryStack);
 

84c38b8711c613567a8e11301c0a440b500bc031
Joshua Wiggins <joshua_kar@hotmail.com>
2019-01-10 15:07:43 -0400

    beginning modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import {
+	View,
+	Text,
+	StyleSheet,
+	StatusBar,
+	Button,
+	ScrollView
+} from "react-native";
+
+const modal = () => {
+	return (
+		<View style={styles.modal}>
+			<Text>Modal</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default modal;

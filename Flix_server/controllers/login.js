99e2fab9cf082fda0f1becd27260876d63c2dc97
Joshua Wiggins <joshua_kar@hotmail.com>
2019-01-15 12:46:51 -0400

    users are now added to db

--- Flix_server/controllers/login.js
+++ Flix_server/controllers/login.js
@@ -6,6 +6,7 @@ const User = require("../models/user");
 exports.postSignup = (req, res, next) => {
 	const validationErrors = validationResult(req).array();
 
+	//check to ensure user details are valid
 	if (validationResult(req).isEmpty()) {
 		const validationMessages = validationErrors.map(
 			validate => validate.msg
@@ -15,21 +16,30 @@ exports.postSignup = (req, res, next) => {
 		});
 	}
 
-	//add user to db
-	bcrypt
-		.hash(req.body.password, 10)
-		.then(hash => {
-			const user = User({
-				name: req.body.name,
-				email: req.body.email,
-				password: hash
+	//Process to add user to db
+	User.find({ email: req.body.email }, (err, result) => {
+		//check for existing user
+		if (result.length) {
+			return res.status(500).json({
+				message: "User with that email already exists!"
 			});
-			user.save(() => res.status(201).json({ message: "User created" }));
-		})
-		.catch(err => next(err));
-	//fist check for existence
-	//no exist then encrypt password
-	//create user and add to db
+		}
+
+		//go ahead with adding user
+		bcrypt
+			.hash(req.body.password, 10)
+			.then(hash => {
+				const user = User({
+					name: req.body.name,
+					email: req.body.email,
+					password: hash
+				});
+				user.save(() =>
+					res.status(201).json({ message: "User created" })
+				);
+			})
+			.catch(err => next(err));
+	});
 };
 
 exports.postLogin = (req, res, next) => {

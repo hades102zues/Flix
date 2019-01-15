b4a785bd34bb5d61ca30cebbb4779381e347b244
Joshua Wiggins <joshua_kar@hotmail.com>
2019-01-15 12:01:56 -0400

    added validation on signup route

--- Flix_server/controllers/login.js
+++ Flix_server/controllers/login.js
@@ -1,5 +1,16 @@
+const { validationResult } = require("express-validator/check");
+
 exports.postSignup = (req, res, next) => {
-	res.send("hi");
+	const validationErrors = validationResult(req).array();
+
+	if (validationErrors.length) {
+		const validationMessages = validationErrors.map(
+			validate => validate.msg
+		);
+		return res.status(500).json({
+			message: validationMessages
+		});
+	}
 };
 
 exports.postLogin = (req, res, next) => {
--- Flix_server/routes/login.js
+++ Flix_server/routes/login.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
-
 const loginControllers = require("../controllers/login");
 
-router.post("/signup", loginControllers.postSignup);
+const { body } = require("express-validator/check");
+
+router.post(
+	"/signup",
+	[
+		body("email")
+			.isEmail()
+			.withMessage("Please enter a valid email address!"),
+		body("password")
+			.isLength({ min: 5 })
+			.withMessage(
+				"Please enter a password that is atleast 5 characters long"
+			),
+		body("confirmPassword").custom((value, { req }) => {
+			if (value !== req.body.password)
+				throw new Error(
+					"Check your Passwords. Both Passwords entered must Match!"
+				);
+		})
+	],
+	loginControllers.postSignup
+);
+
 router.post("/login", loginControllers.postLogin);
 
 module.exports = router;
